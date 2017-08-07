14ce200f540f8229aa4c5f25ac4334f0741d8e9f
Andrey Vakhterov <dpohvar@gmail.com>
2017-08-07 15:07:13 +0500

    + метод insert свойство scrollIntoViewIfNeeded

--- dist/SmartInput.js
+++ dist/SmartInput.js
@@ -375,13 +375,14 @@ SmartInput.prototype.destroy = function destroy(){
  * @param {boolean} [options.deleteContents]
  * @param {boolean} [options.selectStart]
  * @param {boolean} [options.selectEnd]
+ * @param {boolean} [options.scrollIntoViewIfNeeded]
  * @returns {SmartInput}
  */
 SmartInput.prototype.insert = function insert(text, options) {
     if (!this.isInRange()) return this;
     var elements = textToSoftDOMList(text);
     if (!elements.length) return this;
-    if (arguments.length < 2) options = {};
+    if (!options) options = {};
     try {
         var lastElement = elements[elements.length-1];
         var selection = window.getSelection();
@@ -396,11 +397,20 @@ SmartInput.prototype.insert = function insert(text, options) {
             selection.removeAllRanges();
             selection.addRange(range);
         }
+        if (options.scrollIntoViewIfNeeded) scrollElement(lastElement);
         editor.updateValue();
     } catch (ignored){}
     return this;
-
 };
+
+function scrollElement(element){
+    console.log("SCROLL", element, typeof element.scrollIntoViewIfNeeded === "function");
+    if (typeof element.scrollIntoViewIfNeeded === "function") {
+        element.scrollIntoViewIfNeeded(true);
+    } else {
+        scrollIntoViewIfNeeded.call(element, true)
+    }
+}
 
 SmartInput.prototype.isInRange = function isInRange() {
     try {
@@ -728,6 +738,72 @@ function addBrIfNeeded(element){
     var brElement = document.createElement("BR");
     brElement.setAttribute("type", "auto");
     element.appendChild(brElement);
+}
+
+function scrollIntoViewIfNeeded(centerIfNeeded) {
+    function withinBounds(value, min, max, extent) {
+        if (false === centerIfNeeded || max <= value + extent && value <= min + extent) {
+            return Math.min(max, Math.max(min, value));
+        } else {
+            return (min + max) / 2;
+        }
+    }
+
+    function makeArea(left, top, width, height) {
+        return  { "left": left, "top": top, "width": width, "height": height
+            , "right": left + width, "bottom": top + height
+            , "translate":
+                function (x, y) {
+                    return makeArea(x + left, y + top, width, height);
+                }
+            , "relativeFromTo":
+                function (lhs, rhs) {
+                    var newLeft = left, newTop = top;
+                    lhs = lhs.offsetParent;
+                    rhs = rhs.offsetParent;
+                    if (lhs === rhs) {
+                        return area;
+                    }
+                    for (; lhs; lhs = lhs.offsetParent) {
+                        newLeft += lhs.offsetLeft + lhs.clientLeft;
+                        newTop += lhs.offsetTop + lhs.clientTop;
+                    }
+                    for (; rhs; rhs = rhs.offsetParent) {
+                        newLeft -= rhs.offsetLeft + rhs.clientLeft;
+                        newTop -= rhs.offsetTop + rhs.clientTop;
+                    }
+                    return makeArea(newLeft, newTop, width, height);
+                }
+        };
+    }
+
+    var parent, elem = this, area = makeArea(
+        this.offsetLeft, this.offsetTop,
+        this.offsetWidth, this.offsetHeight);
+    while ((parent = elem.parentNode) instanceof HTMLElement) {
+        var clientLeft = parent.offsetLeft + parent.clientLeft;
+        var clientTop = parent.offsetTop + parent.clientTop;
+
+        // Make area relative to parent's client area.
+        area = area.
+        relativeFromTo(elem, parent).
+        translate(-clientLeft, -clientTop);
+
+        parent.scrollLeft = withinBounds(
+            parent.scrollLeft,
+            area.right - parent.clientWidth, area.left,
+            parent.clientWidth);
+
+        parent.scrollTop = withinBounds(
+            parent.scrollTop,
+            area.bottom - parent.clientHeight, area.top,
+            parent.clientHeight);
+
+        // Determine actual scroll amount by reading back scroll properties.
+        area = area.translate(clientLeft - parent.scrollLeft,
+            clientTop - parent.scrollTop);
+        elem = parent;
+    }
 }
 return SmartInput;
 }));
